3259cde15acc234c41091f60e9f07b282fb15415
Arkadiusz Kondas <arkadiusz.kondas@gmail.com>
2016-04-03 22:13:42 +0200

    working js example benchmark

--- javascript/iris.js
+++ javascript/iris.js
@@ -9,7 +9,7 @@ function Benchmark()
 
         var index = 0;
         var total = 0;
-        var endTime = Date.now() + (10 * 1000);
+        var endTime = Date.now() + (30 * 1000);
 
         while (Date.now() < endTime) {
             if (index >= this.attributes.length) {
@@ -22,19 +22,47 @@ function Benchmark()
             total++;
         }
 
-        return 4455;
+        return total;
     }
 
     this.load = function() {
-
+        var fs  = require("fs");
+        var readLine = (function (line) {
+            var row = line.split(',');
+            this.attributes.push(row.slice(0, 4))
+            this.classes.push(row[4]);
+        }).bind(this);
+        fs.readFileSync('iris.csv').toString().split('\n').forEach(readLine);
     }
 
     this.classify = function(index) {
 
-        return 1;
+        var closest_neighbour_index = null;
+        var closest_neighbour_distance = Number.POSITIVE_INFINITY;
+
+        for (var i = 0; i < this.attributes.length; i++) {
+            if (i == index) {
+                continue;
+            }
+
+            var total = 0;
+            for (var j = 0; j < 3; j++) {
+                total += Math.pow(this.attributes[index][j] - this.attributes[i][j], 2);
+            }
+            var distance = Math.sqrt(total);
+
+            if (distance < closest_neighbour_distance) {
+                closest_neighbour_index = i;
+                closest_neighbour_distance = distance;
+            }
+        }
+
+        return this.classes[closest_neighbour_index];
     }
 
 }
+
+console.log('NodeJS ' + process.version);
 
 var benchmark = new Benchmark();
 var total = benchmark.run();
